20514cd00a08e23a6219a4b318d827e682de05f1
sthamizhselvan <s.thamizhselvan@gmail.com>
2020-07-30 17:37:36 -0500

    Changed the Contact form labels

--- src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
+++ src/main/java/com/edurekademo/tutorial/addressbook/ContactForm.java
@@ -25,10 +25,10 @@ public class ContactForm extends FormLayout {
 
     Button save = new Button("Save", this::save);
     Button cancel = new Button("Cancel", this::cancel);
-    TextField firstName = new TextField("First name");
+    TextField firstName = new TextField("First Name");
     TextField lastName = new TextField("Last name");
-    TextField phone = new TextField("Phone");
-    TextField email = new TextField("Email");
+    TextField phone = new TextField("Phone Number");
+    TextField email = new TextField("Email ID");
     DateField birthDate = new DateField("DOB");
 
     Contact contact;
